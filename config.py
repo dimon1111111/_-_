9d80ad8056f87ea4fac6b8a67649dcb7765e1870
dimon1111111 <149920791+dimon1111111@users.noreply.github.com>
2025-02-23 21:13:53 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import random
 import subprocess
 import os
 
-token = 'your token'
+token = '7425805100:AAE7wmDZcSslup3KcPCDWGq32GG2lZ69d2A'
 
 
 
--- logic.py
+++ logic.py
@@ -11,6 +11,13 @@ Memory = defaultdict(list)
 
 class TextAnalysis():   
     memory = defaultdict(list)
+    qwestions = {
+        "как тебя зовут": "Я супер-крутой-бот и мое предназначение помогать тебе!",
+        "сколько тебе лет": "Это слишком философский вопрос",
+        "привет": "Здравствуйте!",
+        "hello": "Hello!",
+        "hi": "Hi!"
+    }
 
     def __init__(self, text, owner):
         self.text = text
@@ -20,20 +27,24 @@ class TextAnalysis():
         TextAnalysis.memory[self.owner].append(self)
     # Задание №1
 
-    def __init__(self, text, owner):
+    #def __init__(self, text, owner): #убрал дублирующий __init__
 
         # Задание №2
-        TextAnalysis.memory[owner]. append(self)
+        #TextAnalysis.memory[owner]. append(self) #эта строка уже есть в первом __init__
     def get_answer(self):
-        res = self.translate("I don't know how to help", "en", "ru") 
-        return res
+        # Задание №6
+        if self.text.lower() in TextAnalysis.qwestions.keys():
+            self.response = TextAnalysis.qwestions[self.text.lower()]
+            return self.response
+        else:
+            res = self.__translate("I don't know how to help", "en", "ru")
+            return res
 
-    def __translate(elf, text, from_lang, to_lang) :
+    def __translate(self, text, from_lang, to_lang):
         try:
-            translator = Translator(from_lang, to_lang)
+            translator = Translator(from_lang=from_lang, to_lang=to_lang) #исправил порядок аргументов
             translated_text = translator.translate(text)
             return translated_text
         except Exception as e:
             print(f"Error during translation:{e}")
             return "Перевод не удался"
-
